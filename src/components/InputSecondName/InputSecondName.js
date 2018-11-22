eb0b091f64eaa5778230cede92975ce5618d16b9
Piotr Tarnowski <ptarnowski@M-PARTYKA.opgkzi.local>
2018-11-22 14:46:34 +0100

    Usunięcie zbędnych komponentów

--- src/components/InputSecondName/InputSecondName.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-
-const InputSecondName = () =>
-  <div>
-    <input className="mb-2" placeholder="Nazwisko" type="text"/>
-  </div>
-
-export default InputSecondName;
--- src/components/PreviewPicture/PreviewPicture.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-
-const PreviewPicture = ({pictureURL}) =>
-  <div>
-    <img src={pictureURL}/>
-  </div>
-
-export default PreviewPicture;
